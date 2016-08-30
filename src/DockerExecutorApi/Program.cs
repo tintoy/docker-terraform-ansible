3afa9b5a637b64fb1d4da9b111879b13be590cad
tintoy <tintoy@tintoy.io>
2016-08-30 10:52:47 +1000

    Initial commit.

---
+++ src/DockerExecutorApi/Program.cs
@@ -0,0 +1,34 @@
+﻿using Docker.DotNet;
+using System;
+using System.Threading;
+using Docker.DotNet.Models;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SynchronizationContext.SetSynchronizationContext(
+                new SynchronizationContext()
+            );
+            try
+            {
+                DockerClientConfiguration config = new DockerClientConfiguration(
+                    new Uri("unix:///var/run/docker.sock")
+                );
+                DockerClient client = config.CreateClient();
+
+                var images = client.Images.ListImagesAsync(new ImagesListParameters()).Result;
+                foreach (var image in images)
+                {
+                    Console.WriteLine(image.ID);
+                }
+            }
+            catch (Exception unexpectedError)
+            {
+                Console.WriteLine(unexpectedError);
+            }
+        }
+    }
+}
